af1101c8665c393bde0a5ed9c27a9ef9b5bab3c7
prashanth-ach <prashanth.ach@gmail.com>
2017-02-10 10:56:35 -0600

    added project

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connStr);
+            CloudTableClient client = storageAccount.CreateCloudTableClient();
+            CloudTable table = client.GetTableReference("employee");
+        
+            TableQuery<message> query = new TableQuery<message>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal,"2"));
+            foreach (message entity in table.ExecuteQuery(query))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+                    entity.Id, entity.Name,entity.Last);
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+        }
+
+           
+    }
+    }
+
---
+++ ConsoleApplication1/ConsoleApplication1/message.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.WindowsAzure.StorageClient;
+    using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ConsoleApplication1
+
+{
+        public class message : TableEntity
+        {
+            public message()
+            {
+            }
+            public message(int id, string name, string last)
+            {
+                Id = id;
+                Name = name;
+                Last = last;
+                PartitionKey = id.ToString();
+                RowKey = name;
+            }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Last { get; set; }
+        }
+    }
